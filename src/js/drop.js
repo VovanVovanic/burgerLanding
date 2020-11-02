6866e00abaa262aa4508a2a512e2adb208cc6754
Vladimir <vladimirplotnikov371@gmail.com>
2020-11-02 21:41:20 +0200

    fixed scripts

--- src/js/drop.js
+++ src/js/drop.js
@@ -2,12 +2,17 @@
   const openTrigger = document.querySelector(".hamburger");
   const mobileMenu = document.querySelector(".drop-menu");
   const closeTrigger = document.querySelector(".close");
+  const menuItem = document.querySelectorAll(".navigation__item--drop");
 
+  menuItem.forEach((el) => {
+    el.addEventListener('click', () => {
+      mobileMenu.classList.remove("drop-menu--active");
+    })
+  })
   openTrigger.addEventListener("click", function(e) {
     e.preventDefault();
 
       if (!mobileMenu.classList.contains("drop-menu--active"))
-        console.log("click");
       mobileMenu.classList.add("drop-menu--active");
   });
 
